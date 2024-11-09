44db78481d131511ad234d9a5a9826bc29c644a8
Sameer-77 <123528477+Sameer-77@users.noreply.github.com>
2024-11-09 08:19:14 +0530

    Create DuplicateNum.cpp

---
+++ ARRAYS/DuplicateNum.cpp
@@ -0,0 +1,22 @@
+
+Problem Link : https://leetcode.com/problems/find-the-duplicate-number/description/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+        slow=nums[slow];
+        fast=nums[nums[fast]];
+        while(slow!=fast) {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }
+        slow=nums[0];
+        while(slow!=fast) {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return fast;
+    }
+};
