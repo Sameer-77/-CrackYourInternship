16feccfa46f2d6a72bbe92731042df6adedd3cab
Sameer-77 <123528477+Sameer-77@users.noreply.github.com>
2024-11-09 09:44:39 +0530

    Create BuyAndSellStock.cpp

---
+++ ARRAYS/BuyAndSellStock.cpp
@@ -0,0 +1,15 @@
+
+//Problem Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0, mini=prices[0];
+        for(int i=1; i<prices.size(); i++) {
+            int cost=prices[i]-mini;
+            profit=max(profit,cost);
+            mini=min(mini,prices[i]);
+        }
+        return profit;
+    }
+};
